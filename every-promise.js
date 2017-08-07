34ed5d7adc61773c493c24c2a5542bae1f5bbb3f
Spinelli Valentinuzzi <1@Mubot.com>
2017-08-07 19:26:41 -0400

    Update every-promise.js

--- every-promise.js
+++ every-promise.js
@@ -2,26 +2,11 @@ Promise.every = function(promises, callback) {
   var len;
   const preserved = new Array((len=promises.length));
   const resolved = [], rejected = [];
-<<<<<<< HEAD
-<<<<<<< HEAD
-  (function recurs(found) {
-    promises[found].then(good => {
-      preserved[found] = good;
-      resolved.push(good);
-=======
-=======
->>>>>>> parent of 78cdfb0... allow concating when minifiying
   (function recurs(found){
     promises[found].then((good, bad) => {
       preserved[found] = good ? resolved.push(good) && good : rejected.push(bad) && bad;
->>>>>>> parent of 78cdfb0... allow concating when minifiying
       if(++found === len) callback(preserved, resolved, rejected);
       else recurs(found)
-    }).catch(bad => {
-      preserved[found] = bad;
-      rejected.push(bad)
-      if(++found === len) callback(preserved, resolved, rejected);
-      else recurs(found)
-   })
+    })
   })(0)
 }
