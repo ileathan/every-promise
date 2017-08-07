ea6f510fce558e573909e3110a11aba5039f8dc3
Spinelli Valentinuzzi <1@Mubot.com>
2017-08-07 18:45:29 -0400

    Update every-promise.js

--- every-promise.js
+++ every-promise.js
@@ -1,17 +1,12 @@
 module.exports = function(promises, callback) {
-  var found = 0;
-  const preserved = new Array(promises.length);
-  const resolved = [];
-  const rejected = [];
-  for(let i = 0, l = promises.length; i < l; i++) {
-    promises[i].then(good => {
-      resolved.push(good);
-      preserved[i] = good;
-      if(++found === l - 1) callback(preserved, resolved, rejected);
-    }).catch(bad => {
-      rejected.push(bad)
-      preserved[i] = bad;
-      if(++found === l - 1) callback(preserved, resolved, rejected);
-   })
-  }
+  var len;
+  const preserved = new Array((len=promises.length));
+  const resolved = [], const rejected = [];
+  (function recurs(found){
+    promises[found].then((good, bad) => {
+      preserved[found] = good ? resolved.push(good) && good : rejected.push(bad) && bad;
+      if(++found === len) callback(preserved, resolved, rejected);
+      else recurs(found)
+    })
+  })(0)
 }
