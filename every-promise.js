78cdfb019674e2d9a9a2cbccf34ae8cf50f9dcd9
leathan <1@leathan.xyz>
2017-08-07 18:57:45 -0400

    allow concating when minifiying

--- every-promise.js
+++ every-promise.js
@@ -1,12 +1,12 @@
 module.exports = function(promises, callback) {
   var len;
-  const preserved = new Array((len=promises.length));
+  const preserved = new Array((len = promises.length));
   const resolved = [], rejected = [];
-  (function recurs(found){
+  (function recurs(found) {
     promises[found].then((good, bad) => {
       preserved[found] = good ? resolved.push(good) && good : rejected.push(bad) && bad;
       if(++found === len) callback(preserved, resolved, rejected);
       else recurs(found)
     })
   })(0)
-}
+};
